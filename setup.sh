8b42d91c8d892e74eaab2ea61da4f3cc2353722f
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-07 03:07:39 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,10 +16,11 @@ sudo apt remove iptables-persistent -y
 sudo ufw disable
 sudo iptables -F
 
-#sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/bibica.net/main/php_vm_standard_a1_flex.ini -O /etc/php/7.4/fpm/php.ini
-#sudo service php7.4-fpm restart
-#sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Webinoly-Optimization/master/my_vm_standard_a1_flex.cnf -O /etc/mysql/my.cnf
-#sudo service mysql restart
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly-OneinStack/main/php.ini -O /etc/php/7.4/fpm/php.ini
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly-OneinStack/main/www.conf -O /etc/php/7.4/fpm/pool.d/www.conf
+sudo service php7.4-fpm restart
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly-OneinStack/main/my.cnf -O /etc/mysql/my.cnf
+sudo service mysql restart
 
 sudo apt update && sudo apt upgrade -y
 sudo webinoly -verify
