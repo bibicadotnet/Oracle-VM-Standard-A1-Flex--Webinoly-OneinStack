2a762d8acea363e9fa9141bbef9855ec534927d5
bibicadotnet <119997727+bibicadotnet@users.noreply.github.com>
2023-07-08 01:48:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,16 +13,17 @@ sudo apt install screen -y
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/webinoly/master/weby -O weby && sudo chmod +x weby && sudo ./weby -clean
 sudo rm /opt/webinoly/webinoly.conf
 sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly/main/vm_standard_a1_flex.conf -O /opt/webinoly/webinoly.conf
-sudo stack -lemp
+sudo stack -lemp -build=light
+
 sudo apt remove iptables-persistent -y
 sudo ufw disable
 sudo iptables -F
 
-# sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly/main/php.ini -O /etc/php/7.4/fpm/php.ini
-# sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly/main/www.conf -O /etc/php/7.4/fpm/pool.d/www.conf
-# sudo service php7.4-fpm restart
-# sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly/main/my.cnf -O /etc/mysql/my.cnf
-# sudo service mysql restart
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly/main/php.ini -O /etc/php/7.4/fpm/php.ini
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly/main/www.conf -O /etc/php/7.4/fpm/pool.d/www.conf
+sudo service php7.4-fpm restart
+sudo wget --no-check-certificate https://raw.githubusercontent.com/bibicadotnet/Oracle-VM-Standard-A1-Flex-Webinoly/main/my.cnf -O /etc/mysql/my.cnf
+sudo service mysql restart
 
 sudo apt update && sudo apt upgrade -y
 sudo webinoly -verify
